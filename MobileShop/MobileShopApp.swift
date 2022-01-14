7b728b0654f7cb5322de24b74a7dd6c000ca1a30
Abdelrahman Qasim <abdelrahman.qasim@orange.com>
2022-01-14 14:22:39 +0200

    Initial Commit

---
+++ MobileShop/MobileShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileShopApp.swift
+//  MobileShop
+//
+//  Created by AbdelrahmanQasim on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MobileShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
